62abba7f1dae76acebe199fca192ff5bba31eb31
TheSimpsonss <vanshita479@gmail.com>
2025-03-06 18:56:20 +0530

    folder structure created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= 'Generative AI Project',
+    version= '0.0.0', 
+    author= 'Vanshita',
+    author_email= 'vanshita479@gmail.com',
+    packages= find_packages(),
+    install_requires=[]
+)
